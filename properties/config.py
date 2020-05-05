96dbc2e57ae27ba7b774bae8fc1ed755679963c1
Utkarsh Tiwari <utkarshtiwari1504@gmail.com>
2020-05-05 13:40:16 +0530

    adding logging and exception handling

--- properties/config.py
+++ properties/config.py
@@ -1,5 +1,7 @@
 import os
-import  inspect
 
-properties_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'properties')
+process_folder_name = 'json_to_excel'
+properties_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+processing_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), process_folder_name, 'processing')
 excel_file = os.path.join(properties_folder, 'excel_database.xlsx')
+log_config = os.path.join(properties_folder, 'logging_config.yml')
